9496e431466254aea4860e0902e7ae65396f044b
Jordan Scales <scalesjordan@gmail.com>
2012-01-21 16:16:32 -0500

    wiki scraper

---
+++ wiki-scraper/wiki.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+
+require 'nokogiri'
+require 'open-uri'
+require 'cgi'
+
+ROOT_URL = 'http://en.wikipedia.org'
+
+def search_url(query)
+  "http://en.wikipedia.org/w/index.php?search=#{CGI.escape(query)}"
+end
+
+def title_from_url(url)
+  
+  doc = Nokogiri::HTML(open(url))
+  doc.css('h1#firstHeading').first.content
+end
+
+def title_from_query(query)
+  title_from_url search_url(query)
+end
+
+def first_link(url)
+  doc = Nokogiri::HTML(open(url))
+  parenth = 0
+  
+  # cycle through each paragraph
+  doc.css('div.mw-content-ltr > p').each do |p|
+    
+    # in each paragraph, go through each node
+    p.children.each do |c|
+      
+      # if we've found two parentheses, return the next link you see
+      if parenth == 0 or (parenth > 1 and (parenth % 2 == 0))
+        if c.name == 'a'
+          return ROOT_URL + c.attributes["href"].value
+        end
+      end
+    
+      # incremement the number of parentheses we've seen
+      if /\(/ === c.to_s
+        parenth += 1
+      elsif /\)/ === c.to_s
+        parenth += 1
+      end
+    end
+    
+  end
+end
+
+def first_link_from_query(query)
+  first_link search_url(query)
+end
+
+start = ARGV.join(' ')
+url = search_url start
+title = title_from_url url
+puts "1: #{title}"
+count = 2
+
+while title != 'Philosophy'
+  url = first_link url
+  title = title_from_url url
+  puts "#{count}: #{title}: #{url}"
+  count += 1
+end
