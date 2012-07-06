b5da0dd7cd825eebb97ccd2b15babfb71275f7d1
flightcrank <ocaumaxim@gmail.com>
2012-07-06 11:27:08 +1000

    added comments and stuff

--- shoot-me-a-text/flightcrank.c
+++ shoot-me-a-text/flightcrank.c
@@ -4,42 +4,45 @@
 #include <stdio.h>
 #include <string.h>
 
+//find out how many times we need to press a key to get the correct char
+//return a 2 value array with index 0 being the key to press and index 1
+//being the amount of times to press it
 int* char_to_keypress(char c) {
-
+	
+	//array of strings with the array index number, acting at the key pad number
 	char *key_pad[] = {" ","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+	//has to be static to it lasts for the duration of the program, not just duration of the function
 	static int key_amount[2];
 
 	key_amount[0] = 0;
 	key_amount[1] = 0;
-	int i,j,k;
+	int i,j;
 
+	//loops through the keypad numbers
 	for(i = 0; i < 10; i++) {
 		
+		//loop through the strings of the current keypad number
 		for(j = 0; j < strlen(key_pad[i]); j++) {
 		
-			
+			//if we find the char were looking for
 			if (key_pad[i][j] == c) {
 				
-				for (k = 0; k < j + 1; k++) {
-					
-					//printf("%d", i);
-					key_amount[1] = j;
-				}
-				
-				key_amount[0] = i;
-				key_amount[1]++;
+				key_amount[0] = i; //keypad number
+				key_amount[1] = j + 1;//number of times we press key to get the char
 				return key_amount;		
 			}
 		}
 	}
-
+	//char not on keypad
 	return NULL;
 }
 
 int main() {
 
-	char *str = "programthis";
-	
+	char str[101];
+	puts("enter string:");
+	fgets(str,101,stdin);
+
 	int i,j;
 	int last = 0;
 	int *val;
@@ -48,18 +51,20 @@ int main() {
 	
 		val = char_to_keypress(str[i]);
 		
-		if (val[0] == last) {
+		if (val != NULL) {
 			
-			printf(" ");
+			if (val[0] == last) {
+			
+				printf(" ");
+			}
+	
+			for (j = 0; j < val[1]; j++) {
+			
+				printf("%d",val[0]);
+			}
+			
+			last = val[0];
 		}
-
-
-		for (j = 0; j < val[1]; j++) {
-		
-			printf("%d",val[0]);
-		}
-		
-		last = val[0];
 	}
 
 	printf("\n");
